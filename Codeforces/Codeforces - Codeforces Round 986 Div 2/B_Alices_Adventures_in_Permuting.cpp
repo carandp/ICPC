5dd57755db9b0b1520c2c24407c6165d89705cd5
Carlos <c.penaa@uniandes.edu.co>
2024-11-11 21:42:10 -0500

    Codeforces - R986D2 - B

--- Codeforces/Codeforces - Codeforces Round 986 Div 2/B_Alices_Adventures_in_Permuting.cpp
+++ Codeforces/Codeforces - Codeforces Round 986 Div 2/B_Alices_Adventures_in_Permuting.cpp
@@ -4,7 +4,40 @@
 
 using namespace std;
 
+typedef long long ll;
+
+void solve() {
+    ll n, b, c;
+    cin >> n >> b >> c;
+
+    if (c >= n) {
+        cout << n << endl;
+        return;
+    }
+
+    if (b == 0) {
+        if (c < n-2) {
+            cout << "-1" << endl;
+        } else {
+            cout << n-1 << endl;
+        }
+    } else if (n == b && b == c){
+        cout << n << endl;
+    } else {
+        cout << n - max(0LL, ((n-c-1)/b+1)) << endl;
+    }
+}
+
+int t;
+
 int main() {
-    cout << "test";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
     return 0;
 }
