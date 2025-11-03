83029cd1afc8476127966175e7d1cad93c52ee0a
Carlos <c.penaa@uniandes.edu.co>
2025-11-03 00:32:47 -0500

    LeetCode - 1578. Minimum Time to Make Rope Colorful

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ Here you’ll find my solutions to problems from various contests. This is my jo
 
 | Platform | Problems Solved |
 |----------|-----------------|
-| LeetCode | 40              |
+| LeetCode | 41              |
 | Codeforces | 26              |
 
 
---
+++ leetcode/topics/greedy/1578_Minimum_Time_to_Make_Rope_Colorful.cpp
@@ -0,0 +1,75 @@
+//  1578. Minimum Time to Make Rope Colorful
+
+/**
+    @author: carandp
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* types */
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+
+/* functions */
+#define f(i, s, n) for(ll i = s; i < n; i++)
+#define rf(i,n,s) for(ll i=n-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define nl "\n"
+#define DBG(X) cerr << #X << " = " << X << endl;
+#define LINE cerr << " ========================= " << endl;
+
+/* prints */
+template <class T>
+void print_v(const vector<T> &v) {cout << "{"; f(i, 0, (int)v.size()) { cout << v[i]; if (i + 1 != (int)v.size()) cout << ",";}cout << "}";}
+template <class T>
+void print_vv(const vector<vector<T>> &vv) { cout << "{\n"; f(i, 0, (int)vv.size()) { cout << "  "; print_v(vv[i]); if (i + 1 != (int)vv.size()) cout << ","; cout << "\n";} cout << "}";}
+template <class T, size_t N>
+void print_a(const T (&arr)[N]) { cout << "{"; f(i,0,(int)N){ cout << arr[i]; if(i+1!=(int)N) cout << ",";} cout << "}"; }
+template <class T, size_t R, size_t C>
+void print_aa(const T (&mat)[R][C]) { cout << "{\n"; f(i,0,(int)R){ cout << "  {"; f(j,0,(int)C){ cout << mat[i][j]; if(j+1!=(int)C) cout << ",";} cout << "}"; if(i+1!=(int)R) cout << ","; cout << "\n";} cout << "}"; }
+
+/* utils */
+const ll MOD = 1e9 + 7;
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+ll mod_pow(ll a, ll b, ll m = MOD) {ll res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1;} return res;}
+ll mod_inv(ll a, ll m = MOD) { return mod_pow(a, m - 2, m);}
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a;}
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a;}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int cost = 0;
+        int last = 0;
+        for (int i=1;i<colors.size();i++) {
+            if (colors[last] != colors[i]) last = i;
+            else {
+                cost += min(neededTime[last],neededTime[i]);
+                if (neededTime[last] < neededTime[i]) last = i;
+            }
+        }
+        return cost;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    Solution sol;
+    string colors = "a";
+    vi t = {1};
+    // string colors = "aaabbbabbbb";
+    // vi t = {3,5,10,7,5,3,5,5,4,8,1};
+    // string colors = "abaac";
+    // vi t = {1,2,3,4,5};
+    cout << sol.minCost(colors,t) << nl;
+    
+}
