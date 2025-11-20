f0f878727696bb101bf66a662a17c1f6e019155c
Carlos <c.penaa@uniandes.edu.co>
2025-11-20 00:37:37 -0500

    LeetCode - 757. Set Intersection Size At Least Two

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ Welcome to my **ICPC** repo!
 
 | Platform | Problems Solved |
 |----------|-----------------|
-| LeetCode | 57              |
+| LeetCode | 58              |
 | Codeforces | 30              |
 
 
---
+++ leetcode/topics/greedy/757_Set_Intersection_Size_At_Least_Two.cpp
@@ -0,0 +1,90 @@
+//  757. Set Intersection Size At Least Two
+
+/**
+    @author: carandp
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* types */
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+
+/* functions */
+#define f(i, s, n) for(ll i = s; i < n; i++)
+#define rf(i,n,s) for(ll i=n-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define nl "\n"
+#define DBG(X) cerr << #X << " = " << X << endl;
+#define LINE cerr << " ========================= " << endl;
+
+/* prints */
+template <class T>
+void print_v(const vector<T> &v) {cout << "{"; f(i, 0, (int)v.size()) { cout << v[i]; if (i + 1 != (int)v.size()) cout << ",";}cout << "}";}
+template <class T>
+void print_vv(const vector<vector<T>> &vv) { cout << "{\n"; f(i, 0, (int)vv.size()) { cout << "  "; print_v(vv[i]); if (i + 1 != (int)vv.size()) cout << ","; cout << "\n";} cout << "}";}
+template <class T, size_t N>
+void print_a(const T (&arr)[N]) { cout << "{"; f(i,0,(int)N){ cout << arr[i]; if(i+1!=(int)N) cout << ",";} cout << "}"; }
+template <class T, size_t R, size_t C>
+void print_aa(const T (&mat)[R][C]) { cout << "{\n"; f(i,0,(int)R){ cout << "  {"; f(j,0,(int)C){ cout << mat[i][j]; if(j+1!=(int)C) cout << ",";} cout << "}"; if(i+1!=(int)R) cout << ","; cout << "\n";} cout << "}"; }
+
+/* utils */
+const ll MOD = 1e9 + 7;
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+ll mod_pow(ll a, ll b, ll m = MOD) {ll res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1;} return res;}
+ll mod_inv(ll a, ll m = MOD) { return mod_pow(a, m - 2, m);}
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a;}
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a;}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+public:
+    struct cmp {
+        bool operator()(const vector<int>& a, const vector<int>& b) const {
+            if (a[0] != b[0])
+                return a[0] > b[0];  // Descending by start of range
+            return a[1] < b[1]; // Ascending by end of range
+        }
+    };
+
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end(), cmp());
+
+        int x = intervals[0][0];
+        int y = x+1;
+        int res = 2;
+
+        for (int i=1;i<n;i++) { // Greedy approach
+            int l = intervals[i][0];
+            int r = intervals[i][1];
+            if (x >= l && y <= r) continue; // There is nothing to update, range contains x and y
+            if (l < x && r < x) { // Range does not contain x or y
+                x = l;
+                y = l+1;
+                res += 2;
+            } else { // Range does not contain y
+                y = x;
+                x = l;
+                res++;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    Solution sol;
+    vector<vector<int>> x = {{2,7},{1,5},{2,4},{4,6},{0,3},{6,8},{0,8}};
+    cout << sol.intersectionSizeTwo(x) << nl;
+    
+}
