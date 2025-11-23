664f4696febd1dce128cf2958ddd70ea2cd9dcb1
Carlos <c.penaa@uniandes.edu.co>
2025-11-23 01:00:53 -0500

    LeetCode - weekly_contest_477

---
+++ leetcode/topics/prefix_array/3756_Concatenate_Non-Zero_Digits_and_Multiply_by_Sum_II.cpp
@@ -0,0 +1,114 @@
+//  3756. Concatenate Non-Zero Digits and Multiply by Sum II
+
+/**
+    @author: carandp
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* types */
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+
+/* functions */
+#define f(i, s, n) for(ll i = s; i < n; i++)
+#define rf(i,n,s) for(ll i=n-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define nl "\n"
+#define DBG(X) cerr << #X << " = " << X << endl;
+#define LINE cerr << " ========================= " << endl;
+
+/* prints */
+template <class T>
+void print_v(const vector<T> &v) {cout << "{"; f(i, 0, (int)v.size()) { cout << v[i]; if (i + 1 != (int)v.size()) cout << ",";}cout << "}";}
+template <class T>
+void print_vv(const vector<vector<T>> &vv) { cout << "{\n"; f(i, 0, (int)vv.size()) { cout << "  "; print_v(vv[i]); if (i + 1 != (int)vv.size()) cout << ","; cout << "\n";} cout << "}";}
+template <class T, size_t N>
+void print_a(const T (&arr)[N]) { cout << "{"; f(i,0,(int)N){ cout << arr[i]; if(i+1!=(int)N) cout << ",";} cout << "}"; }
+template <class T, size_t R, size_t C>
+void print_aa(const T (&mat)[R][C]) { cout << "{\n"; f(i,0,(int)R){ cout << "  {"; f(j,0,(int)C){ cout << mat[i][j]; if(j+1!=(int)C) cout << ",";} cout << "}"; if(i+1!=(int)R) cout << ","; cout << "\n";} cout << "}"; }
+
+/* utils */
+const ll MOD = 1e9 + 7;
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+ll mod_pow(ll a, ll b, ll m = MOD) {ll res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1;} return res;}
+ll mod_inv(ll a, ll m = MOD) { return mod_pow(a, m - 2, m);}
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a;}
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a;}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+public:
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        long long m = 1e9 + 7;
+        int n = s.size();
+
+        vector<int> prefixSum(n, 0);
+        vector<long long> prefixVal(n, 0);
+        vector<int> prefixCount(n, 0);
+
+        vector<long long> p10(n + 1);
+        p10[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            p10[i] = (p10[i - 1] * 10) % m;
+        }
+
+        prefixSum[0] = s[0] - '0';
+        if (s[0] != '0') {
+            prefixVal[0] = s[0] - '0';
+            prefixCount[0] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            int digit = s[i] - '0';
+            prefixSum[i] = prefixSum[i - 1] + digit;
+            prefixCount[i] = prefixCount[i - 1];
+            prefixVal[i] = prefixVal[i - 1];
+            if (digit != 0) {
+                prefixCount[i]++;
+                prefixVal[i] = (prefixVal[i - 1] * 10 + digit) % m;
+            }
+        }
+
+        vector<int> res;
+        for (auto& q : queries) {
+            int l = q[0];
+            int r = q[1];
+            int sumDigits = prefixSum[r];
+            if (l > 0) sumDigits -= prefixSum[l - 1];
+            long long currVal;
+            if (l == 0) {
+                currVal = prefixVal[r];
+            } else {
+                int len_nz = prefixCount[r] - prefixCount[l - 1];
+                long long leftPart = (prefixVal[l - 1] * p10[len_nz]) % m;
+                currVal = (prefixVal[r] - leftPart) % m;
+                if (currVal < 0) currVal += m;
+            }
+            long long ans = (currVal * sumDigits) % m;
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    Solution sol;
+    // string s = "10203004";
+    // vector<vector<int>> q = {{0,7},{1,3},{4,6}};
+    // string s = "9876543210";
+    // vector<vector<int>> q = {{0,9}};
+    string s = "148";
+    vector<vector<int>> q = {{0,0},{0,1},{0,2},{1,1},{1,2},{2,2}};
+    print_v(sol.sumAndMultiply(s,q));
+    
+}
---
+++ leetcode/topics/string/3754_Concatenate_Non-Zero_Digits_and_Multiply_by_Sum_I.cpp
@@ -0,0 +1,73 @@
+//  3754. Concatenate Non-Zero Digits and Multiply by Sum I
+
+/**
+    @author: carandp
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* types */
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+
+/* functions */
+#define f(i, s, n) for(ll i = s; i < n; i++)
+#define rf(i,n,s) for(ll i=n-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define nl "\n"
+#define DBG(X) cerr << #X << " = " << X << endl;
+#define LINE cerr << " ========================= " << endl;
+
+/* prints */
+template <class T>
+void print_v(const vector<T> &v) {cout << "{"; f(i, 0, (int)v.size()) { cout << v[i]; if (i + 1 != (int)v.size()) cout << ",";}cout << "}";}
+template <class T>
+void print_vv(const vector<vector<T>> &vv) { cout << "{\n"; f(i, 0, (int)vv.size()) { cout << "  "; print_v(vv[i]); if (i + 1 != (int)vv.size()) cout << ","; cout << "\n";} cout << "}";}
+template <class T, size_t N>
+void print_a(const T (&arr)[N]) { cout << "{"; f(i,0,(int)N){ cout << arr[i]; if(i+1!=(int)N) cout << ",";} cout << "}"; }
+template <class T, size_t R, size_t C>
+void print_aa(const T (&mat)[R][C]) { cout << "{\n"; f(i,0,(int)R){ cout << "  {"; f(j,0,(int)C){ cout << mat[i][j]; if(j+1!=(int)C) cout << ",";} cout << "}"; if(i+1!=(int)R) cout << ","; cout << "\n";} cout << "}"; }
+
+/* utils */
+const ll MOD = 1e9 + 7;
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+ll mod_pow(ll a, ll b, ll m = MOD) {ll res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1;} return res;}
+ll mod_inv(ll a, ll m = MOD) { return mod_pow(a, m - 2, m);}
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a;}
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a;}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        string s = to_string(n);
+        long long sum = 0;
+        string x = "";
+        for (int i=0;i<s.size();i++) {
+            if (s[i] != '0') {
+                sum += s[i] - '0';
+                x += s[i];
+            }
+        }
+        long long xLong = 0;
+        if (x != "") xLong = stoll(x);
+        long long res = sum * xLong;
+        return res;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    Solution sol;
+    int n = 10203004;
+    cout << sol.sumAndMultiply(n) << nl;
+    
+}
