b9bb41bb7d548a32a06ad4208f21196e29fae662
Carlos <c.penaa@uniandes.edu.co>
2026-01-11 00:54:49 -0500

    LeetCode - 85. Maximal Rectangle

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ Welcome to my **ICPC** repo!
 
 | Platform   | Problems Solved |
 |------------|-----------------|
-| LeetCode   | 114             |
+| LeetCode   | 115             |
 | Codeforces | 33              |
 
 
---
+++ leetcode/topics/stack/85_Maximal_Rectangle.cpp
@@ -0,0 +1,88 @@
+//  85. Maximal Rectangle
+
+/**
+    @author: carandp
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* types */
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+
+/* functions */
+#define f(i, s, n) for(ll i = s; i < n; i++)
+#define rf(i,n,s) for(ll i=n-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define nl "\n"
+#define DBG(X) cerr << #X << " = " << X << endl;
+#define LINE cerr << " ========================= " << endl;
+
+/* prints */
+template <class T>
+void print_v(const vector<T> &v) {cout << "{"; f(i, 0, (int)v.size()) { cout << v[i]; if (i + 1 != (int)v.size()) cout << ",";}cout << "}";}
+template <class T>
+void print_vv(const vector<vector<T>> &vv) { cout << "{\n"; f(i, 0, (int)vv.size()) { cout << "  "; print_v(vv[i]); if (i + 1 != (int)vv.size()) cout << ","; cout << "\n";} cout << "}";}
+template <class T, size_t N>
+void print_a(const T (&arr)[N]) { cout << "{"; f(i,0,(int)N){ cout << arr[i]; if(i+1!=(int)N) cout << ",";} cout << "}"; }
+template <class T, size_t R, size_t C>
+void print_aa(const T (&mat)[R][C]) { cout << "{\n"; f(i,0,(int)R){ cout << "  {"; f(j,0,(int)C){ cout << mat[i][j]; if(j+1!=(int)C) cout << ",";} cout << "}"; if(i+1!=(int)R) cout << ","; cout << "\n";} cout << "}"; }
+
+/* utils */
+const ll MOD = 1e9 + 7;
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+ll mod_pow(ll a, ll b, ll m = MOD) {ll res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1;} return res;}
+ll mod_inv(ll a, ll m = MOD) { return mod_pow(a, m - 2, m);}
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a;}
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a;}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int> hist(m,0);
+        int res = 0;
+
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<m;j++) {
+                if (matrix[i][j] == '1') hist[j]++;
+                else hist[j] = 0;
+            }
+            stack<int> st;
+            int maxArea = 0;
+            for (int k=0;k<=m;k++) {
+                int h = 0;
+                if (k != m) h = hist[k];
+                while (!st.empty() && h < hist[st.top()]) {
+                    int height = hist[st.top()];
+                    st.pop();
+                    int width = k;
+                    if (!st.empty()) width = k - st.top() - 1;
+                    maxArea = max(maxArea, height * width);
+                }
+                st.push(k);
+            }
+            res = max(res, maxArea);
+        }
+        return res;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    Solution sol;
+    // vi x = {1, 2, 3, 1};
+    // cout << sol.method(x) << nl;
+    
+}
